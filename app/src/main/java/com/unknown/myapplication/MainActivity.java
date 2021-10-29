5cddcddd4a1b89f44777fab4e4b51e06cdd5c86e
thieugiakk-hhp <thieugiakk.hhp@gmail.com>
2021-10-29 21:14:24 +0700

    Initial commit

---
+++ app/src/main/java/com/unknown/myapplication/MainActivity.java
@@ -0,0 +1,36 @@
+package com.unknown.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleButton toggleButton;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toggleButton = findViewById(R.id.toggleButton);
+        textView = findViewById(R.id.textView);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    startService(new Intent(MainActivity.this, MyService.class));
+                } else {
+                    stopService(new Intent(MainActivity.this, MyService.class));
+                }
+            }
+        });
+    }
+}
